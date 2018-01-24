26b85728795d24dd9d9a29fb99f3b674dc6980c6
danl411412 <daan.b.lee@gmail.com>
2018-01-24 09:12:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(arr)# code an argument here
   # Your code here
   arr.any? do |word|
     word.length > 4
-  end 
+  end
 end
 
 def find_the_cheese# code an argument here
