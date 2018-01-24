8a7a26c9c36b97f79d7dcac4883374ffb0018a93
danl411412 <daan.b.lee@gmail.com>
2018-01-24 09:11:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(arr)# code an argument here
   # Your code here
   arr.collect do |planeteer|
-    "#{planeteer.captialize}!"
+    "#{planeteer.capitalize}!"
   end
 end
 
